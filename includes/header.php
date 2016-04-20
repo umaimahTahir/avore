e7444fd96590a7379144d412606913976f452f3d
arsalan <arsalanwahid@hotmail.com>
2016-04-20 18:42:16 +0500

    removed javascript related links ,pasted in footer file and added a some stick footer contect from line 57

--- includes/header.php
+++ includes/header.php
@@ -1,10 +1,57 @@
-<!-- Latest compiled and minified CSS -->
-<link rel="stylesheet" href="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css">
+<!DOCTYPE html>
+<html lang="en">
 
-<!-- jQuery library -->
-<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.0/jquery.min.js"></script>
+<head>
 
-<!-- Latest compiled JavaScript -->
-<script src="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js"></script>
+    <!-- Latest compiled and minified CSS -->
+    <link rel="stylesheet" href="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css">
 
+
+
+    <!--Set encoding to utf-8 standard-->
+    <meta charset="utf-8">
+
+    <!--Tells browser to use latest version of Internet Explorer settings-->
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+
+    <!--set page to device width and zoom level 1-->
+    <meta name="viewport" content="width=device-width,initial-scale=1">
     <title>Avore</title>
+
+    <style>
+        .panel-footer {
+            margin-bottom: -20px;
+        }
+
+        .jumbotron {
+            margin-top: -20px;
+        }
+
+        * {
+            margin: 0;
+        }
+
+        html,
+        body {
+            height: 100%;
+        }
+
+        .wrapper {
+            min-height: 100%;
+            height: auto !important;
+            height: 100%;
+            margin: 0 auto -4em;
+        }
+
+        .footer,
+        .push {
+            height: 4em;
+        }
+    </style>
+
+</head>
+
+<body>
+
+    <?php include 'includes\nav.php'; ?>
+    <div class="wrapper">
