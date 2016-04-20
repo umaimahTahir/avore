2029b6b079642013810256cd8497eb7ef42b8eae
arsalan <arsalanwahid@hotmail.com>
2016-04-20 18:40:45 +0500

    add footer file to project

---
+++ includes/footer.php
@@ -0,0 +1,22 @@
+<!-- jQuery library -->
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.0/jquery.min.js"></script>
+
+<!-- Latest compiled JavaScript -->
+<script src="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js"></script>
+
+<!--include this file at the bottom of every page as part of the template-->
+
+<div class="push"></div>
+</div>
+<div class="footer">
+<div class="panel panel-default">
+  <div class="panel-body">
+      <a href="contactus.php">Contact us</a>
+  </div>
+  <div class="panel-footer">&copy;2016 AVORE ALL RIGHTS RESERVED</div>
+</div>
+</div>
+    </body>
+
+
+</html>
