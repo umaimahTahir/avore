0a3bf354a8018c5abbb79726fa69e7e33143f8dc
arsalan <arsalanwahid@hotmail.com>
2016-04-20 18:41:06 +0500

    update page with new header ,footer settings

--- contactus.php
+++ contactus.php
@@ -1,13 +1,10 @@
  <?php include 'includes\header.php'?>
-<?php include 'includes\nav.php'; ?>
 
-<html>
-<body>
-<div class="jumbotron">
-    <p>AVORE</p>
-    </div>
-    <div class="container">
-        <p>Email:xyz.com</p>
-    </div>
-    </body>
-<html>
+
+
+
+<ul>
+    <li>HEllo</li>
+    </ul>
+
+<?php include 'includes\footer.php'?>
